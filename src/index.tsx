045c9f47e4e790f57474e3bd210d2a2c5a90cecf
Egor Rudenko <egor.rudenko.msc@gmail.com>
2025-02-02 17:48:34 +0300

    scroll 1

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,15 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 
+document
+  .querySelector("meta[name=viewport]")
+  .setAttribute(
+    "content",
+    "width=device-width, user-scalable=no, initial-scale=" +
+      1 / window.devicePixelRatio +
+      ""
+  );
+
 import applyTheme from "./theme";
 
 applyTheme();
